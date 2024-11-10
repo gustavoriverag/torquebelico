3cf2ed9f7bf860c7be300b9f6c32ff5c57091460
Juan-salvo <juan.f.salvo@gmail.com>
2024-11-10 17:03:45 -0300

    agregar calculo para radiocontrol, falta calibrar

--- src/torquebelico/torquebelico.ino
+++ src/torquebelico/torquebelico.ino
@@ -13,12 +13,15 @@ int pin_pwm_2 = 11;
 int empuje = 0;
 int direccion = 0;
 
+int M1 = 0;
+int M2 = 0;
+
 // Read the number of a given channel and convert to the range provided.
 // If the channel is off, return the default value
 int readChannel(int channelInput, int minLimit, int maxLimit, int defaultValue){
   int ch = pulseIn(channelInput, HIGH, 30000);
   if (ch < 100) return defaultValue;
-  return map(ch, 1000, 2000, minLimit, maxLimit);
+  return map(ch, 1100, 1900, minLimit, maxLimit);
 }
 
 // Read the channel and return a boolean value
@@ -33,18 +36,51 @@ void setup() {
   Serial.begin(9600);
   pinMode(CH1, INPUT);
   pinMode(CH2, INPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  pinMode(pin_mot_1, OUTPUT);
+  pinMode(pin_mot_1_r, OUTPUT);
+  pinMode(pin_mot_2, OUTPUT);
+  pinMode(pin_mot_2_r, OUTPUT);
+  pinMode(pin_pwm_1, OUTPUT);
+  pinMode(pin_pwm_2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   empuje = readChannel(CH1, -255, 255, 0);
-  direccion = readChannel(CH2, -1, 1, 0);
+  direccion = readChannel(CH2, -255, 255, 0);
+
+
+  // calcular empuje para cada motor
+  if (abs(empuje) > 100){
+  // caso 1: empuje alto => manda empuje
+  
+    M1 = min(255, 255 + direccion)*empuje/255;
+    M2 = min(255, 255 - direccion)*empuje/255;
+  }
+  
+  else {
+    
+  // caso 2: empuje bajo => manda direccion
+    M1 = direccion/2 + empuje;
+    M2 = -direccion/2 + empuje;
+  }
+
+  //mandar señal correspondiente
+  digitalWrite(pin_mot_1, (M1>=0));
+  digitalWrite(pin_mot_1_r, (M1<0));
+  digitalWrite(pin_mot_2, (M2>=0));
+  digitalWrite(pin_mot_2_r, (M2<0));
+  
+  analogWrite(pin_pwm_1, abs(M1));
+  analogWrite(pin_pwm_2, abs(M2));
+
+  //
+
+  
+  Serial.print("CH1: ");
+  Serial.println(pulseIn(CH1, HIGH));
+  
   Serial.print("CH2: ");
-  Serial.println(pulseIn(CH3, HIGH));
-  delay(500);
+  Serial.println(pulseIn(CH2, HIGH));
 }
